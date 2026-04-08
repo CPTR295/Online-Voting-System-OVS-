1bfa1183e4f100dea11290aafed00c3a438052b9
Tushar Reddy <reddytushar295@outlook.com>
2026-04-08 10:12:52 +0530

    Added API Repos

---
+++ API/app/repository/candidates.py
@@ -0,0 +1,67 @@
+from typing import Dict, List,Any 
+from sqlalchemy import update,delete,insert
+from sqlalchemy.future import select
+from sqlalchemy.orm import Session
+from app.model.db import Candidate
+from datetime import datetime
+
+class CandidateRepository:
+    def __init__(self,sess:Session):
+        self.sess = sess 
+    
+    async def insert_candidate(self,candidate:Candidate)->bool:
+        try:
+            sql =insert(Candidate).values(elect_id=candidate.elect_id,cand_id=candidate.cand_id,
+                                          firstname=candidate.firstname,lastname=candidate.lastname,middlename=candidate.middlename,
+                                          address = candidate.address, tel = candidate.tel, position=candidate.position,
+                                          party = candidate.party, filing_date=datetime.strptime(candidate.filing_date,'%Y-%m-%d').date())
+            await self.sess.execute(sql)
+            await self.sess.commit()
+            await self.sess.close()
+            return True
+        except Exception as e:
+            print(e)
+        return False
+    
+    async def update_candidate(self,id:int,details:Dict[str,Any])->bool:
+        try:
+            sql = update(Candidate).where(Candidate.id==id).values(**details)
+            await self.sess.execute(sql)
+            await self.sess.commit()
+            await self.sess.close()
+            return True
+        except Exception as e:
+            print(e)
+        return False
+
+    async def delete_candidate(self,id:int)->bool:
+        try:
+            sql = delete(Candidate).where(Candidate.id==id)
+            await self.sess.execute(sql)
+            await self.sess.commit()
+            await self.sess.close()
+            return True
+        except Exception as e:
+            print(e)
+        return False
+    
+    async def select_all_candidate(self):
+        try:
+            sql = select(Candidate)
+            sql.execution_options(synchronize_session="fetch")
+            q = await self.sess.execute(sql)
+            records = q.scalars().all()
+            await self.sess.close()
+            return records
+        except Exception as e:
+            print(e)
+    
+    async def select_one_candidate(self,id:int):
+        sql = select(Candidate).where(Candidate.id==id)
+        sql.execution_options(synchronize_session="fetch")
+        q = await self.sess.execute(sql)
+        records = q.scalars().all()
+        await self.sess.close()
+        return records
+
+        
